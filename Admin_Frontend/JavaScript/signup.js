09237750df5b1022745bb509137a5f880f75150e
Gopal Krushna Sahoo <gopalkrushnas063@gmail.com>
2023-02-27 14:03:37 +0530

    updated

--- Admin_Frontend/JavaScript/signup.js
+++ Admin_Frontend/JavaScript/signup.js
@@ -29,7 +29,7 @@ const postSignupFetch = async () => {
     if (response.ok) {
         signup.reset();
         alert("Admin Saved Successfully!");
-        window.location = "../index.html";
+        window.location = "/Admin_Frontend/logout_index.html";
     } else {
         let res = await response.JSON();
         alert(res.message);
